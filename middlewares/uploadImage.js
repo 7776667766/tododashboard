d80eabfc51e97ee2e6a0064c30977d923c59a69b
Ahmad MUjtaba <ranagee444732@gmail.com>
2023-12-15 03:01:10 -0800

    services get images wip

--- middlewares/uploadImage.js
+++ middlewares/uploadImage.js
@@ -1,3 +1,17 @@
+// const multer = require('multer');
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'uploads/');
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, Date.now() + '-' + file.originalname);
+//   }
+// });
+
+// const upload = multer({ storage: storage });
+// module.exports=upload
+
 const multer = require('multer');
 
 const storage = multer.diskStorage({
@@ -5,9 +19,11 @@ const storage = multer.diskStorage({
     cb(null, 'uploads/');
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname);
+    const sanitizedFileName = file.originalname.replace(/\s+/g, '_');
+    const fullFileName = `${Date.now()}-${sanitizedFileName}`;
+    cb(null, fullFileName);
   }
 });
-
 const upload = multer({ storage: storage });
-module.exports=upload
+
+module.exports = upload;
