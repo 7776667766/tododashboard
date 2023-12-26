997ec754eae837561ad1e1a1c5aabd2d37d5214d
Ahmad MUjtaba <ranagee444732@gmail.com>
2023-12-26 17:29:55 +0500

    stripe payment method done

--- controllers/CardController.js
+++ controllers/CardController.js
@@ -1,50 +1,50 @@
-const forge = require('node-forge');
-const Card = require('../models/CardModel');
-
-const encryptData = (data, encryptionKey) => {
-    const keyBuffer = forge.util.createBuffer(forge.util.hexToBytes(encryptionKey));
-
-    const cipher = forge.cipher.createCipher('AES-CBC', keyBuffer);
-
-    const iv = forge.random.getBytesSync(16);
-
-    cipher.start({ iv });
-
-    cipher.update(forge.util.createBuffer(data, 'utf-8'));
-
-    cipher.finish();
-
-    return forge.util.encode64(cipher.output.getBytes());
-};
-
-const encryptionKey = 'aabbccddeeff00112233445566778899';
-const encryptedData = encryptData('mySensitiveData', encryptionKey);
-console.log('Encrypted Data:', encryptedData);
-
+const Card = require('../models/StripeModal');
+const stripe = require('stripe')('sk_test_51NX2rxKZnNaiPBqB5BbVKBBCRFKZ60D6gHoEaJa0etfZIR2B5rArHDA154NYvHtXo39dwXYuFd51sdNHF2N0jyu200Cl2Su7WS');
 
 const addCardApi = async (req, res, next) => {
     try {
-        const { name, credit, days, cvv } = req.body;
-        console.log(req.body)
+        const { name, token } = req.body;
+        console.log(token,"token")
 
-        if (!name) {
-            return res.status(400).json(
-                { status: "error", message: "Invalid CardHolderName" }
-            );
+        if (!name || !token) {
+            return res.status(400).json({ status: 'error', message: 'Invalid request payload' });
         }
-        
+        const creditCardInfo = req.body.token;
+        console.log(creditCardInfo,"information of card")
 
-        const encodedCardNumber = encryptData(credit, encryptionKey);
-        const encodedCVV = encryptData(cvv, encryptionKey);
+        if (!creditCardInfo ) {
+            return res.status(400).json({ status: 'error', message: 'Invalid credit card information in the token' });
+        }
 
-        console.log("ENCODED CARD && ENCODED CVV", "", encodedCardNumber, encodedCVV)
+        // const creditCardToken = creditCardInfo.id;
+        // console.log(creditcardToken,"tken of card")
+
+
+        const paymentMethod = await stripe.paymentMethods.create({
+            type: 'card',
+            card: {
+                token:creditCardInfo,
+            },
+        });
+
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: 1000,
+            currency: 'usd',
+            payment_method: paymentMethod.id,
+            confirmation_method: 'manual',
+            confirm: true,
+            return_url: 'https://www.youtube.com', // Replace with your actual return URL
+
+        });
 
         const newCard = await Card.create({
             name,
-            credit: encodedCardNumber,
-            days,
-            cvv: encodedCVV,
+            stripePaymentMethodId: paymentMethod.id,
+            stripePaymentIntentId: paymentIntent.id,
         });
+
+        console.log("newCard", newCard)
+
 
         res.status(201).json({ status: 'success', data: newCard });
     } catch (error) {
---
+++ models/StripeModal.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Cardholder Name is required'],
+    trim: true,
+  },
+
+  stripePaymentMethodId: {
+    type: String,
+    required: true,
+  },
+
+  stripePaymentIntentId: {
+    type: String,
+    required: true,
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+
+  deletedAt: {
+    type: Date,
+  },
+});
+
+const Card = mongoose.model('Card', cardSchema);
+
+module.exports = Card;
