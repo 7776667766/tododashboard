b9f5c3de6bedb3e3003b9967fdc0e4f80aabda1e
Ahmad MUjtaba <ranagee444732@gmail.com>
2023-12-28 17:55:03 +0500

    transection dashbord done

--- models/CardModel.js
+++ models/CardModel.js
@@ -12,14 +12,31 @@ const cardSchema = new mongoose.Schema({
     required: true,
   },
 
-  // stripePaymentIntentId: {
-  //   type: String,
-  //   required: true,
-  // },
+  stripeCustomerId: {
+    type: String
+  },
+  stripeSubscriptionId: {
+    type: String
+  },
+  check: {
+    type: String,
 
-  subscriptionPlan :{
-    type:String,
+  },
+  subscriptionPlan: {
+    type: String,
 
+  },
+  amount: {
+    type: Number
+  },
+  cardType: {
+    type: String, 
+  },
+  cardDigits: {
+    type: String, 
+  },
+  expiryDate: {
+    type: String, 
   },
   createdAt: {
     type: Date,
--- routes/CardRoutes.js
+++ routes/CardRoutes.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
-// const auth = require("../middlewares/auth");
 
 const {
   addCardApi, 
+  getTransactionDetails
 } = require("../controllers/CardController");
 
 router.post("/card/add", addCardApi);
+router.get("/transaction/get", getTransactionDetails);
 
 module.exports = router;
