23dde3567dcd03eed5607edf25750cccb600ac2f
Muhammad Tahir <m.tahiridrees27@gmail.com>
2024-01-03 18:08:29 +0500

    Updating

--- util/imgFullPath.js
+++ util/imgFullPath.js
@@ -1,6 +1,7 @@
 const imgFullPath = (imgPath) => {
   if (!imgPath || imgPath === null) return "";
   if (imgPath.startsWith("http")) return imgPath;
+  // return "http://192.168.18.16:3000/" + imgPath;
   return process.env.SERVER_URL + imgPath;
 };
 module.exports = imgFullPath;
