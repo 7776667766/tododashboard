2aae0ef29779b18103ed31d28b5d48f7e94257b4
BilalAulakh <bb807562@gmail.com>
2024-02-13 16:04:01 +0500

    changes

--- controllers/ReviewsController.js
+++ controllers/ReviewsController.js
@@ -74,6 +74,215 @@ const getAllReviews = (req, res) => {
           "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg",
         deletedAt: null,
       },
+      {
+        name: "Review 9",
+        message: "This is a test review 2.",
+        rating: 2.0,
+        image:
+          "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg",
+        deletedAt: null,
+      },
+      {
+        name: "Review 10",
+        message: "This is a test review 2.",
+        rating: 5.0,
+        image:
+          "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg",
+        deletedAt: null,
+      },
+      {
+        name: "Review 11",
+        message: "This is a test review 2.",
+        rating: 3.0,
+        image:
+          "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg",
+        deletedAt: null,
+      },
+      {
+        name: "Review 12",
+        message: "This is a test review 2.",
+        rating: 2.0,
+        image:
+          "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg",
+        deletedAt: null,
+      },
+      {
+        name: "Review 13",
+        message: "This is a test review 2.",
+        rating: 1.0,
+        image:
+          "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg",
+        deletedAt: null,
+      },
+      {
+        name: "Review 14",
+        message: "This is a test review 2.",
+        rating: 3.0,
+        image:
+          "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg",
+        deletedAt: null,
+      },
+      {
+        name: "Review 15",
+        message: "This is a test review 2.",
+        rating: 2.0,
+        image:
+          "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg",
+        deletedAt: null,
+      },
+      {
+        name: "Review 16",
+        message: "This is a test review 2.",
+        rating: 5.0,
+        image:
+          "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg",
+        deletedAt: null,
+      },
+      {
+        name: "Review 17",
+        message: "This is a test review 2.",
+        rating: 2.0,
+        image:
+          "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg",
+        deletedAt: null,
+      },
+      {
+        name: "Review 18",
+        message: "This is a test review 2.",
+        rating: 3.5,
+        image:
+          "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg",
+        deletedAt: null,
+      },
+      {
+        name: "Review 19",
+        message: "This is a test review 2.",
+        rating: 3.0,
+        image:
+          "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg",
+        deletedAt: null,
+      },
+      {
+        name: "Review 20",
+        message: "This is a test review 2.",
+        rating: 4.0,
+        image:
+          "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg",
+        deletedAt: null,
+      },
+      {
+        name: "Review 21",
+        message: "This is a test review 2.",
+        rating: 4.0,
+        image:
+          "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg",
+        deletedAt: null,
+      },
+      {
+        name: "Review 22",
+        message: "This is a test review 2.",
+        rating: 4.0,
+        image:
+          "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg",
+        deletedAt: null,
+      },
+      {
+        name: "Review 23",
+        message: "This is a test review 2.",
+        rating: 4.0,
+        image:
+          "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg",
+        deletedAt: null,
+      },
+      {
+        name: "Review 24",
+        message: "This is a test review 2.",
+        rating: 4.0,
+        image:
+          "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg",
+        deletedAt: null,
+      },
+      {
+        name: "Review 25",
+        message: "This is a test review 2.",
+        rating: 4.0,
+        image:
+          "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg",
+        deletedAt: null,
+      },
+      {
+        name: "Review 26",
+        message: "This is a test review 2.",
+        rating: 4.0,
+        image:
+          "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg",
+        deletedAt: null,
+      },
+      {
+        name: "Review 27",
+        message: "This is a test review 2.",
+        rating: 4.0,
+        image:
+          "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg",
+        deletedAt: null,
+      },
+      {
+        name: "Review 28",
+        message: "This is a test review 2.",
+        rating: 4.0,
+        image:
+          "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg",
+        deletedAt: null,
+      },
+      {
+        name: "Review 29",
+        message: "This is a test review 2.",
+        rating: 4.0,
+        image:
+          "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg",
+        deletedAt: null,
+      },
+      {
+        name: "Review 30",
+        message: "This is a test review 2.",
+        rating: 4.0,
+        image:
+          "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg",
+        deletedAt: null,
+      },
+      {
+        name: "Review 31",
+        message: "This is a test review 2.",
+        rating: 4.0,
+        image:
+          "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg",
+        deletedAt: null,
+      },
+      {
+        name: "Review 32",
+        message: "This is a test review 2.",
+        rating: 4.0,
+        image:
+          "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg",
+        deletedAt: null,
+      },
+      {
+        name: "Review 33",
+        message: "This is a test review 2.",
+        rating: 4.0,
+        image:
+          "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg",
+        deletedAt: null,
+      },
+      {
+        name: "Review 34",
+        message: "This is a test review 2.",
+        rating: 4.0,
+        image:
+          "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg",
+        deletedAt: null,
+      },
+    
       // {
       //   name: "Review 3",
       //   message: "not bad.",
--- util/imgFullPath.js
+++ util/imgFullPath.js
@@ -2,7 +2,7 @@ const imgFullPath = (imgPath) => {
   if (!imgPath || imgPath === null) return "";
   if (imgPath.startsWith("http")) return imgPath;
   // return "http://192.168.18.25:3002/" + imgPath;
-  // return "http://192.168.18.138:3000/" + imgPath;
+  return "http://192.168.18.27:3000/" + imgPath;
   return process.env.SERVER_URL + imgPath;
 };
 module.exports = imgFullPath;
