39d86be1cb409f2268914518c6f0a4e8aaa3b14b
BilalAulakh <bb807562@gmail.com>
2024-06-25 17:33:20 +0500

    changes

--- models/BusinessRequest.js
+++ models/BusinessRequest.js
@@ -4,7 +4,6 @@ const businessSchema = new mongoose.Schema({
   ownerId:{
     type: mongoose.Schema.Types.ObjectId,
     ref: "Owner",
-    required: [true, "Owner ID is required"],
   },
   description: {
     type: String,
