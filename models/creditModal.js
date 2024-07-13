d50a6555a886b762c96bd07c54e1e9a6c9f37790
BilalAulakh <bb807562@gmail.com>
2024-07-13 17:14:25 +0500

    fsd

--- models/creditModal.js
+++ models/creditModal.js
@@ -1,25 +1,29 @@
 const mongoose = require("mongoose");
 
 const creditSchema = new mongoose.Schema({
-    amount: {
-        type: String,
-        required: [true, "Credit Amount is required"],
-        trim: true,
-    },
-    credit:{
-        type:String,
-    },
-    email: {
-        type: String,
-        trim: true,
-    },
-    createdAt: {
-        type: Date,
-        default: new Date(),
-    },
-    deletedAt: {
-        type: Date,
-    },
+  amount: {
+    type: String,
+    required: [true, "Credit Amount is required"],
+    trim: true,
+  },
+  credit: {
+    type: String,
+  },
+  email: {
+    type: String,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+  deletedAt: {
+    type: Date,
+  },
+  status: {
+    type: Date,
+    default: true,
+  },
 });
 
 module.exports = mongoose.model("Credit", creditSchema);
